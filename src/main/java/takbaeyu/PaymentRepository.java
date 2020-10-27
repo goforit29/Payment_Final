5f625e90546f74c48bb2b4ab0a3a1a58988178df
goforit29 <goforit29@gmail.com>
2020-10-27 17:57:19 +0900

    first commit

---
+++ src/main/java/takbaeyu/PaymentRepository.java
@@ -0,0 +1,8 @@
+package takbaeyu;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PaymentRepository extends PagingAndSortingRepository<Payment, Long>{
+
+
+}
